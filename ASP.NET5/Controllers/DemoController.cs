fa8a427a9fd255edb51531ec238e81352947a183
Flypans <sbkwon011@outlook.com>
2024-08-28 19:08:22 +0100

    Add index.cshtml to Views folder with Razor syntax for MVC pattern

---
+++ ASP.NET5/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET5.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Demo Page");
+            return View();
+        }
+    }
+}
